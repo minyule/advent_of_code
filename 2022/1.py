69d7065c6d2335217699f2869a6d9bfaa4839766
min4849 <min4849@naver.com>
2022-12-02 13:48:30 +0900

    1st part2

--- 2022/1.py
+++ 2022/1.py
@@ -6,6 +6,7 @@ num = 1
 res_num = 1
 total_max = 0
 one_sum = 0
+sum_lst = []
 
 while True:
     try:
@@ -15,6 +16,7 @@ while True:
             if total_max < one_sum:
                 total_max = one_sum
                 res_num = num
+            sum_lst.append(one_sum)
             one_sum = 0
             num += 1
 
@@ -25,4 +27,10 @@ while True:
     except EOFError:
         break
 
-print(total_max, res_num)
+# part 1 - ans: 69177
+print("Part 1: ", total_max)
+
+# part 2 - ans: 207456
+sum_lst.sort()
+s = sum_lst[-1] + sum_lst[-2] + sum_lst[-3]
+print("Part 2: ", s)
